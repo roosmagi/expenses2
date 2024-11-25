ae0a1a168caacc5f32054bf26da7979d71728463
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-11-25 15:36:59 +0200

    Ülesanne 4 - Andmete suunamine (bottom-up) ja väärtuste hetkeseisu lugemine ning määramine

--- src/assets/Expenses/Expenses.jsx
+++ src/assets/Expenses/Expenses.jsx
@@ -1,28 +1,44 @@
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-import './Expenses.css'
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpenseFilter.jsx';
+import Card from '../UI/Card';
+import './Expenses.css';
 
-const Expenses = (props) => {
+const Expenses = () => {
+  const [filteredYear, setFilteredYear] = useState('2024');
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New book',
+      price: 30.99,
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New jeans',
+      price: 99.99,
+    }
+  ];
 
-    const expenses =[  
-        {
-          date: new Date(2024, 10, 12),
-          title: 'New book',
-          price: 30.99 
-        },
-        {
-          date: new Date(2024, 10, 12),
-          title: "New jeans",
-          price: 99.99
-        } 
-      ]
-        
-        return (
-          <Card className="expenses">
-            <ExpenseItem data={expenses[0]}/>
-            <ExpenseItem data={expenses[1]}/>
-          </Card>
-        )
-} 
+  const onChangeHandler = (selectedYear) => {
+    console.log('Valitud aasta: ',typeof selectedYear); 
+    setFilteredYear(selectedYear);
+  };
 
-export default Expenses
+  const filteredExpenses = expenses.filter(expenses => 
+    expenses.date.getFullYear().toString() === filteredYear
+  );
+  
+
+  return (
+    <div>
+      <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeHandler} />
+        {filteredExpenses.map((expense, index) => (
+          <ExpenseItem key={index} data={expense} />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
